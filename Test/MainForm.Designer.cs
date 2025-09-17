8013a95fc2edc9b80d46046c5933bcdff5edc0a5
Lander <55660647+FelixLeander@users.noreply.github.com>
2025-09-17 22:46:13 +0200

    cleanup

--- Test/MainForm.Designer.cs
+++ Test/MainForm.Designer.cs
@@ -28,36 +28,37 @@
         /// </summary>
         private void InitializeComponent()
         {
-            button1 = new Button();
-            pictureBox1 = new PictureBox();
+            buttonPickPixelDelayed = new Button();
+            pictureBoxPickedColor = new PictureBox();
             button2 = new Button();
-            pictureBox2 = new PictureBox();
-            button3 = new Button();
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).BeginInit();
+            pictureBoxRecordedColor = new PictureBox();
+            buttonRecordColor = new Button();
+            buttonPickColor = new Button();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPickedColor).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxRecordedColor).BeginInit();
             SuspendLayout();
             // 
-            // button1
+            // buttonPickPixelDelayed
             // 
-            button1.Location = new Point(110, 119);
-            button1.Name = "button1";
-            button1.Size = new Size(218, 23);
-            button1.TabIndex = 0;
-            button1.Text = "Pick point in 3sec";
-            button1.UseVisualStyleBackColor = true;
-            button1.Click += Button1_Click;
+            buttonPickPixelDelayed.Location = new Point(12, 12);
+            buttonPickPixelDelayed.Name = "buttonPickPixelDelayed";
+            buttonPickPixelDelayed.Size = new Size(218, 23);
+            buttonPickPixelDelayed.TabIndex = 0;
+            buttonPickPixelDelayed.Text = "Pick point in 3sec";
+            buttonPickPixelDelayed.UseVisualStyleBackColor = true;
+            buttonPickPixelDelayed.Click += Button1_Click;
             // 
-            // pictureBox1
+            // pictureBoxPickedColor
             // 
-            pictureBox1.Location = new Point(12, 158);
-            pictureBox1.Name = "pictureBox1";
-            pictureBox1.Size = new Size(776, 280);
-            pictureBox1.TabIndex = 1;
-            pictureBox1.TabStop = false;
+            pictureBoxPickedColor.Location = new Point(12, 41);
+            pictureBoxPickedColor.Name = "pictureBoxPickedColor";
+            pictureBoxPickedColor.Size = new Size(768, 138);
+            pictureBoxPickedColor.TabIndex = 1;
+            pictureBoxPickedColor.TabStop = false;
             // 
             // button2
             // 
-            button2.Location = new Point(400, 119);
+            button2.Location = new Point(647, 12);
             button2.Name = "button2";
             button2.Size = new Size(133, 23);
             button2.TabIndex = 2;
@@ -65,49 +66,65 @@
             button2.UseVisualStyleBackColor = true;
             button2.Click += Button2_Click;
             // 
-            // pictureBox2
+            // pictureBoxRecordedColor
             // 
-            pictureBox2.Location = new Point(12, 51);
-            pictureBox2.Name = "pictureBox2";
-            pictureBox2.Size = new Size(776, 32);
-            pictureBox2.TabIndex = 3;
-            pictureBox2.TabStop = false;
-            pictureBox2.MouseClick += PictureBox2_MouseClick;
+            pictureBoxRecordedColor.Dock = DockStyle.Bottom;
+            pictureBoxRecordedColor.Location = new Point(0, 250);
+            pictureBoxRecordedColor.Name = "pictureBoxRecordedColor";
+            pictureBoxRecordedColor.Size = new Size(792, 32);
+            pictureBoxRecordedColor.TabIndex = 3;
+            pictureBoxRecordedColor.TabStop = false;
+            pictureBoxRecordedColor.MouseClick += PictureBox2_MouseClick;
             // 
-            // button3
+            // buttonRecordColor
             // 
-            button3.Location = new Point(12, 22);
-            button3.Name = "button3";
-            button3.Size = new Size(75, 23);
-            button3.TabIndex = 4;
-            button3.Text = "button3";
-            button3.UseVisualStyleBackColor = true;
-            button3.Click += Button3_Click;
+            buttonRecordColor.AutoSize = true;
+            buttonRecordColor.Location = new Point(12, 219);
+            buttonRecordColor.Name = "buttonRecordColor";
+            buttonRecordColor.Size = new Size(86, 25);
+            buttonRecordColor.TabIndex = 4;
+            buttonRecordColor.Text = "Record Color";
+            buttonRecordColor.UseVisualStyleBackColor = true;
+            buttonRecordColor.Click += Button3_Click;
+            // 
+            // buttonPickColor
+            // 
+            buttonPickColor.AutoSize = true;
+            buttonPickColor.Location = new Point(371, 185);
+            buttonPickColor.Name = "buttonPickColor";
+            buttonPickColor.Size = new Size(104, 25);
+            buttonPickColor.TabIndex = 5;
+            buttonPickColor.Text = "buttonPickColor";
+            buttonPickColor.UseVisualStyleBackColor = true;
+            buttonPickColor.Click += ButtonSetColor_Click;
             // 
             // MainForm
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Controls.Add(button3);
-            Controls.Add(pictureBox2);
+            ClientSize = new Size(792, 282);
+            Controls.Add(buttonPickColor);
+            Controls.Add(buttonRecordColor);
+            Controls.Add(pictureBoxRecordedColor);
             Controls.Add(button2);
-            Controls.Add(pictureBox1);
-            Controls.Add(button1);
+            Controls.Add(pictureBoxPickedColor);
+            Controls.Add(buttonPickPixelDelayed);
             Name = "MainForm";
             Text = "Form1";
             Load += MainForm_Load;
-            ((System.ComponentModel.ISupportInitialize)pictureBox1).EndInit();
-            ((System.ComponentModel.ISupportInitialize)pictureBox2).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxPickedColor).EndInit();
+            ((System.ComponentModel.ISupportInitialize)pictureBoxRecordedColor).EndInit();
             ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
 
-        private Button button1;
-        private PictureBox pictureBox1;
+        private Button buttonPickPixelDelayed;
+        private PictureBox pictureBoxPickedColor;
         private Button button2;
-        private PictureBox pictureBox2;
-        private Button button3;
+        private PictureBox pictureBoxRecordedColor;
+        private Button buttonRecordColor;
+        private Button buttonPickColor;
     }
 }
